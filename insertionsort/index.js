5ed9a51ecb2c71e8d7cf52248462eb19a21aa286
Eljahon <neljahon@gmail.com>
2022-10-10 19:03:48 +0500

    add stirng class

--- insertionsort/index.js
+++ insertionsort/index.js
@@ -1,1 +1,18 @@
-let array=[1,2,3,4,4,5,5345,345,435,345,534,554,34435,43,5434,53,45,3,5,5,3,5];
+let array = [
+  1, 2, 3, 4, 4, 5, 5345, 345, 435, 345, 534, 554, 34435, 43, 5434, 53, 45, 3,
+  5, 5, 3, 5,
+];
+function insertionSort(array) {
+  let len = array.length;
+  for (let i = 1; i < len; i++) {
+    let current = array[i];
+    let j;
+    for (j = i - 1; j >= 0 && array[j] > current; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = current;
+  }
+  return array;
+}
+
+console.log(insertionSort(array));
