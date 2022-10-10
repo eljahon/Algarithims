f3b11916d7031c1bd79d2ed4b7805751e3d9524c
Eljahon <neljahon@gmail.com>
2022-10-10 19:13:48 +0500

    add string class

--- recursion/index.js
+++ recursion/index.js
@@ -1,11 +1,19 @@
 // Recursion orqali n sonigacha bo'lagan sonlarining raqamlar yig'indsi va ko'paymasni toping ;
-function recursionSum (n) {
-    if (n === 0) return 0;
-    return n+recursionSum(n-1);
-};
-console.log(recursionSum(10));
-function recursionMultipl (n) {
-    if (n === 1) return 1;
-    return n*recursionMultipl(n-1);
-};
-console.log(recursionMultipl(10))
+// function recursionSum (n) {
+//     if (n === 0) return 0;
+//     return n+recursionSum(n-1);
+// };
+// console.log(recursionSum(10));
+// function recursionMultipl (n) {
+//     if (n === 1) return 1;
+//     return n*recursionMultipl(n-1);
+// };
+// console.log(recursionMultipl(10))
+ function fib (n, a,b) {
+    let s = a+b;
+    console.log(s);
+    if (n < 0) return 
+    return fib(n-1, b, a+b)
+ }
+;
+console.log(fib(5, 0,1));
