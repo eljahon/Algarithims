aea390d820443eabfa8554cb0c616e123f2fdb58
Eljahon <neljahon@gmail.com>
2022-12-26 23:53:03 +0500

    list add

---
+++ class/index.js
@@ -0,0 +1,9 @@
+class Abstract {
+  constructor() {
+    if (this.constructor === "Abstract") {
+      throw new Error(" this is class Abstract");
+    }
+  }
+};
+const abstract = new Abstract ();
+console.log(abstract);
