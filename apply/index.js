89337c77a841d7a451431410a03add7512c20acd
Eljahon <neljahon@gmail.com>
2023-02-09 10:55:14 +0500

    new method add applay

---
+++ apply/index.js
@@ -0,0 +1,24 @@
+const computer = {
+          name: 'MacBook',
+          isOn: false,
+          turnOn() {
+              this.isOn = true;
+              return `The ${this.name} is On`;
+          },
+          turnOff() {
+              this.isOn = false;
+              return `The ${this.name} is Off`;
+          }
+      };
+      const server = {
+          name: 'Dell PowerEdge T30',
+          isOn: false
+      };
+      let result = computer.turnOn.apply(server);
+
+console.log(result);
+let arr = [1, 2, 3];
+let numbers = [4,5, 6];
+arr.push.apply(arr, numbers);
+let max = Math.max.call(...arr)
+console.log(arr, max); 
